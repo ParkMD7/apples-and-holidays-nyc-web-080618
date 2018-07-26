8a5641fa59e1026b6d891e5197aac8cc21fdbe67
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-26 05:02:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_supplies_in_holidays(holiday_hash)
 
   holiday_hash.each do |season, supply|
     puts "#{season.capitalize}:"
-      season.each do 
+      holidays.each do |holiday, supplies| 
 
 
 end
