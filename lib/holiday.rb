8f2f8db506ef3483d5eae2817da922532a61ec6f
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-26 04:20:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,6 +30,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   
   holiday_supplies[:winter][:christmas] = "booze"
+  holiday_supplies[:winter][:new_years] = "booze"
 
 end
 
