43543114bf306a472e6889891be261910543f151
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-26 04:34:25 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,6 +24,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
   
 end
 
+
+
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
@@ -35,6 +37,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 end
 
 
+
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
@@ -43,6 +46,8 @@ def add_supply_to_memorial_day(holiday_hash, supply)
    
 end
 
+
+
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
@@ -50,14 +55,16 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
   
-  
-  holiday_hash
 end
+
+
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 
 end
+
+
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
@@ -69,6 +76,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
 end
+
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
